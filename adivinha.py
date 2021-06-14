ffe78e6fe577f606bd90d6b6bf9dfe909c6dfd46
Leandro Pestana <leandro.pestana@Leandros-MacBook-Air.local>
2021-06-13 23:20:36 -0300

    Primeiro Jogo de Adivinha

---
+++ adivinha.py
@@ -0,0 +1,12 @@
+print("Jogo do Adivinha")
+
+numero_secreto = 40
+
+chute_str = input("Digite um número")
+print("Você Digitou", chute_str)
+
+chute = int(chute_str)
+if(numero_secreto == chute):
+    print("Você acertou, parabens!")
+else:
+    print("Você errou, tente novamente")
